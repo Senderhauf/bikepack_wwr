0023c1afaa7f998c96bbb8a632dda9e6072773aa
Senderhauf <sinterhauf@gmail.com>
2021-03-14 12:47:45 -0500

    wiki setup with first sqlbackup

---
+++ wiki/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+apt update;
+
+##########################################
+# install automysqlbackup and run backup
+apt install automysqlbackup -y;
+FILE=wiki_backup.sql.gz;
+# if backup file exists restore db
+if test -f "$FILE"; then
+	# copy sql backup script to docker container
+	docker cp $FILE wiki:/$FILE;
+
+	# run bash command in container to restore database from automysqlbackup
+	docker exec wiki /bin/bash gunzip < $FILE | mysql -u wiki_user -p w1k1p455w0rd;
+fi;
+##########################################
+
+
+##########################################
+# install packages necessary for file metadata processing
+apt install -y ghostscript poppler-utils
+##########################################
+
---
+++ wiki/media/favicon/readme.md
@@ -0,0 +1,5 @@
+#Source
+ 
+Favicon and logo sourced from https://www.designevo.com/apps/logo/?name=mountain-and-vector-gear
+
+Used site colors 
